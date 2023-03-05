7276855dd5697f07aaf205eb76ea9568ddd9d235
Sovraska <simon.novikov@gmail.com>
2023-03-05 14:18:02 +0300

    ALLOWED_HOSTS. add

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -2,7 +2,10 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('У меня получилось! ДА РЕальн оДЕплой по пушу РОБИТ  АЛИлуя')
+    return HttpResponse(
+        'У меня получилось! ДА РЕальн '
+        'оДЕплой по пушу РОБИТ  АЛИлуя'
+    )
 
 
 def second_page(request):
